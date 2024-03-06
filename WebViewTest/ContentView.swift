c9384f040c9330f9411d2bef5e9cf4121ff568ef
n-hidaka <85332716+n-hidaka@users.noreply.github.com>
2024-03-06 11:50:42 +0900

    feat: use WebView

--- WebViewTest/ContentView.swift
+++ WebViewTest/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(loardUrl: URL(string: "https://www.example.com")!)
     }
 }
 
