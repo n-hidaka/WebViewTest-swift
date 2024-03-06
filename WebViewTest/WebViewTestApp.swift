88f30f28bf7553eeba66cb451d7635ece1e63e21
n-hidaka <85332716+n-hidaka@users.noreply.github.com>
2024-03-06 10:14:14 +0900

    Initial Commit

---
+++ WebViewTest/WebViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTestApp.swift
+//  WebViewTest
+//
+//  Created by Naoto Hidaka on 2024/03/06.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
